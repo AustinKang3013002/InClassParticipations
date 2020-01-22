5f4d60315f1f9e8eaf3989697bc6cca7dea98045
kang0016 <kang0016@PCB-LAB360-06>
2020-01-22 11:25:51 -0600

    Method Class Add created

--- InClassExamples/DataAccess/Program.cs
+++ InClassExamples/DataAccess/Program.cs
@@ -11,7 +11,25 @@ namespace DataAccess
     {
         static void Main(string[] args)
         {
-
+            double result= Add(5, 17.5);
+            Console.WriteLine($"5+17.5 = { result.ToString("N2")}");
+            Console.ReadKey();
+            
+            
         }
+        /// <summary>
+        /// sums up two numbers and provides the results 
+        /// </summary>
+        /// <param name="val1">Left hand operator</param>
+        /// <param name="val2">Right hand operator</param>
+        /// <returns>The sum of val1 and val2</returns>
+        //method Add
+        static double Add(double val1, double val2)
+        {
+            double sum = val1 + val2;
+            return sum;
+        }
+
+
     }
 }
