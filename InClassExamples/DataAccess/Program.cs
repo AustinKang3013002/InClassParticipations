c5da98a830b0da693a221e8c6616c02f7e06899a
kang0016 <kang0016@PCB-LAB360-06>
2020-01-22 11:09:52 -0600

    Created Solution

---
+++ InClassExamples/DataAccess/Program.cs
@@ -0,0 +1,17 @@
+﻿// Austin Kang
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
